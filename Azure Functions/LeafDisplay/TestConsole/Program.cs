28114d870b716b031eff2850f3960f7538fa8582
Hein Andre Grønnestad <hag@haugnett.no>
2018-03-19 20:43:42 +0100

    Debug.WriteLine -> Console.WriteLine

--- Azure Functions/LeafDisplay/TestConsole/Program.cs
+++ Azure Functions/LeafDisplay/TestConsole/Program.cs
@@ -13,37 +13,38 @@ namespace TestConsole {
 
             try {
 
-                Debug.WriteLine("Logging in...");
+                Console.WriteLine("Logging in...");
                 var loginResult = await lc.LogIn();
 
-                Debug.WriteLine("Requesting battery status check...");
+                Console.WriteLine("Requesting battery status check...");
                 var requestBatteryStatusCheckResult = await lc.RequestBatteryStatusCheck();
 
-                Debug.WriteLine("Waiting for battery status check to finish...");
+                Console.WriteLine("Waiting for battery status check to finish...");
                 var wait = await lc.WaitForBatteryStatusCheckResult(requestBatteryStatusCheckResult.ResultKey);
 
-                Debug.WriteLine("Getting battery status record...");
+                Console.WriteLine("Getting battery status record...");
                 var getBatteryStatusRecordResult = await lc.GetBatteryStatusRecord();
 
-                Debug.WriteLine($"SoC Record: {getBatteryStatusRecordResult?.BatteryStatusRecord?.BatteryStatus?.StateOfCharge?.Percent}%");
-                Debug.WriteLine($"Updated: {getBatteryStatusRecordResult?.BatteryStatusRecord?.NotificationDateAndTime}");
+                Console.WriteLine($"SoC Record: {getBatteryStatusRecordResult?.BatteryStatusRecord?.BatteryStatus?.StateOfCharge?.Percent}%");
+                Console.WriteLine($"Updated: {getBatteryStatusRecordResult?.BatteryStatusRecord?.NotificationDateAndTime}");
 
             } catch (Exception e) {
-                Debug.WriteLine($"Could not get data from NissanConnect!");
-                Debug.WriteLine($"Error: {e.Message}");
+                Console.WriteLine($"Could not get data from NissanConnect!");
+                Console.WriteLine($"Error: {e.Message}");
 
-                Debug.WriteLine($"Trying to get last battery status record as a fallback...");
+                Console.WriteLine($"Trying to get last battery status record as a fallback...");
 
                 try {
                     var bsr = await lc.GetBatteryStatusRecord();
-                    Debug.WriteLine($"SoC Record: {bsr?.BatteryStatusRecord?.BatteryStatus?.StateOfCharge?.Percent}%");
-                    Debug.WriteLine($"Updated: {bsr?.BatteryStatusRecord?.NotificationDateAndTime}");
+                    Console.WriteLine($"SoC Record: {bsr?.BatteryStatusRecord?.BatteryStatus?.StateOfCharge?.Percent}%");
+                    Console.WriteLine($"Updated: {bsr?.BatteryStatusRecord?.NotificationDateAndTime}");
 
                 } catch (Exception f) {
-                    Debug.WriteLine($"Error: {f.Message}");
+                    Console.WriteLine($"Error: {f.Message}");
                 }
             }
 
+            Console.ReadLine();
         }
     }
 }
