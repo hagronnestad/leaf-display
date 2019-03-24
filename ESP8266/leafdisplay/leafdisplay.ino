f67329ce4b45f48beff51107f8f487bfb06f1060
Hein Andre Grønnestad <hag@haugnett.no>
2019-03-24 23:16:56 +0100

    Added more data, color changes to progress bar, charge icon, clean up

--- ESP8266/leafdisplay/leafdisplay.ino
+++ ESP8266/leafdisplay/leafdisplay.ino
@@ -13,83 +13,95 @@ const uint8_t nextionCommandEnd[3] = {0xFF, 0xFF, 0xFF};
 
 String txtSoC = "txtSoC";
 String txtTimeStamp = "txtTimeStamp";
-String txtChargingStatus = "txtChargeText";
-//String picChargingStatus = "p0";
-//String txtRangeAc = "t3";
-String txtRange = "txtKm";
+String txtChargeText = "txtChargeText";
+String txtChargeIcon = "txtChargeIcon";
+String txtRangeNoAc = "txtRangeNoAc";
+String txtRangeAc = "txtRangeAc";
 String pbSoC = "pbSoC";
-
 
 int socValue = 0;
 String chargingStatus = "";
+String chargingStatusFormatted = "";
 String pluginState = "";
-int range = 0;
+String pluginStateFormatted = "";
+int rangeNoAc = 0;
 int rangeAc = 0;
 String chargeTime = "";
 String timeStamp = "";
 
-
+int redColor = 63488;
+int greenColor = 2016;
+int blueColor = 823;
+int orangeColor = 64512;
 
 // SETTINGS SETUP AND LOADING
 
 #include <EEPROM.h>
-struct Settings {
-  char ssid[32];
-  char password[32];
-  char leafDataUrl[128];
+struct Settings
+{
+    char ssid[32];
+    char password[32];
+    char leafDataUrl[128];
 };
 
 Settings s;
 
-Settings SettingsSetup() {
-  Settings s;
-  
-  EEPROM.begin(512);
-  EEPROM.get(0, s);
-  
-  Serial.println("\n\nType space to enter setup...");
-  Serial.setTimeout(3000);
-  
-  String sm = Serial.readStringUntil('\n');
+Settings SettingsSetup()
+{
+    Settings s;
 
-  if (sm == " ") {
-    Serial.setTimeout(60000);
-    Serial.println("Setup");
-    Serial.println("--------------");
-    
-    Serial.print("Enter new SSID [" + String(s.ssid) + "]: ");
-    String ssid = Serial.readStringUntil('\n');
-    if (ssid.length() > 0) ssid.toCharArray(s.ssid, sizeof(s.ssid));
-    Serial.println(String(s.ssid));
-  
-    Serial.print("Enter new password [" + String(s.password) + "]: ");
-    String password = Serial.readStringUntil('\n');
-    if (password.length() > 0) password.toCharArray(s.password, sizeof(s.password));
-    Serial.println(s.password);
-  
-    Serial.print("Enter new Leaf Data URL [" + String(s.leafDataUrl) + "]: ");
-    String leafDataUrl = Serial.readStringUntil('\n');
-    if (leafDataUrl.length() > 0) leafDataUrl.toCharArray(s.leafDataUrl, sizeof(s.leafDataUrl));
-    Serial.println(s.leafDataUrl);
-  
-    EEPROM.put(0, s);
-    EEPROM.commit();
-    EEPROM.end();
-  
-    Serial.println("\n\nSettings saved!\n");
-    
-  } else {
-    Serial.println("Skipping setup!");
-  }
+    EEPROM.begin(512);
+    EEPROM.get(0, s);
 
-  Serial.setTimeout(1000);
-  return s;
+    Serial.println("\n\nType space to enter setup...");
+    Serial.setTimeout(5000);
+
+    String sm = Serial.readStringUntil('\n');
+
+    if (sm == " ")
+    {
+        Serial.setTimeout(60000);
+        Serial.println("Setup");
+        Serial.println("--------------");
+
+        Serial.print("Enter new SSID [" + String(s.ssid) + "]: ");
+        String ssid = Serial.readStringUntil('\n');
+        if (ssid.length() > 0)
+            ssid.toCharArray(s.ssid, sizeof(s.ssid));
+        Serial.println(String(s.ssid));
+
+        Serial.print("Enter new password [" + String(s.password) + "]: ");
+        String password = Serial.readStringUntil('\n');
+        if (password.length() > 0)
+            password.toCharArray(s.password, sizeof(s.password));
+        Serial.println(s.password);
+
+        Serial.print("Enter new Leaf Data URL [" + String(s.leafDataUrl) + "]: ");
+        String leafDataUrl = Serial.readStringUntil('\n');
+        if (leafDataUrl.length() > 0)
+            leafDataUrl.toCharArray(s.leafDataUrl, sizeof(s.leafDataUrl));
+        Serial.println(s.leafDataUrl);
+
+        EEPROM.put(0, s);
+        EEPROM.commit();
+        EEPROM.end();
+
+        Serial.println("\n\nSettings saved!\n");
+    }
+    else
+    {
+        Serial.println("Skipping setup!");
+    }
+
+    Serial.setTimeout(1000);
+    return s;
 }
 
-
 // Called from Ticker
-void ticker_updateDataFromApi() {
-    if((WiFiMulti.run() != WL_CONNECTED)) {
+void ticker_updateDataFromApi()
+{
+    if ((WiFiMulti.run() != WL_CONNECTED))
+    {
         return;
     }
 
@@ -97,82 +109,82 @@ void ticker_updateDataFromApi() {
     updateScreen();
 }
 
-void updateDataFromApi() {
+void updateDataFromApi()
+{
     HTTPClient http;
 
     http.setTimeout(10000);
-  
+
     http.begin(s.leafDataUrl);
     int httpCode = http.GET();
 
     //Serial.println("HTTP Code: " + String(httpCode));
 
-    if (httpCode < 0) {
+    if (httpCode < 0)
+    {
         //Serial.println("Feil: " + String(http.errorToString(httpCode).c_str()));
         http.end();
         return;
     }
 
-    if(httpCode == HTTP_CODE_OK) {
-        StaticJsonBuffer<1024> jsonBuffer;
-        JsonObject& root = jsonBuffer.parseObject(http.getString());
+    if (httpCode == HTTP_CODE_OK)
+    {
+        StaticJsonBuffer<2048> jsonBuffer;
+        JsonObject &root = jsonBuffer.parseObject(http.getString());
 
         socValue = root["stateOfCharge"];
         chargingStatus = root["chargingStatus"].asString();
-        pluginState = root["pluginState"].asString();
-        range = root["cruisingRangeAcOff"];
+        chargingStatusFormatted = root["chargingStatusFormatted"].asString();
+        pluginState = root["plugInState"].asString();
+        pluginStateFormatted = root["plugInStateFormatted"].asString();
+        rangeNoAc = root["cruisingRangeAcOff"];
         rangeAc = root["cruisingRangeAcOn"];
-        chargeTime = "test";// root["ChargeTime"].asString();
-        timeStamp = root["timestamp"].asString();
+        chargeTime = root["minutesToFull200Formatted"].asString();
+        timeStamp = root["timestampFormatted"].asString();
+    }
+    else
+    {
+        //Serial.println("Feil: " + String(http.errorToString(httpCode).c_str()));
     }
 
     http.end();
 }
 
+void setup()
+{
+    Serial.begin(115200);
 
+    s = SettingsSetup();
 
+    WiFi.disconnect(); // Clears last connected AP from flash
 
-void setup() {
-  Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP((String(s.ssid)).c_str(), (String(s.password).c_str()));
 
-  // Disable Nextion communication
-  //sendToNextion("code_c");
-  //sendToNextion("com_stop");
-  
-  s = SettingsSetup();
+    setText(txtChargeText, "WiFi connecting...");
+    Serial.print("Connecting to: '");
+    Serial.print(s.ssid);
+    Serial.print("', with password: '");
+    Serial.print(s.password);
+    Serial.print("'");
 
-  WiFi.disconnect(); // Clears last connected AP from flash
-  //WiFi.begin((String(s.ssid)).c_str(), (String(s.password).c_str()));
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP((String(s.ssid)).c_str(), (String(s.password).c_str()));
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
 
-  Serial.print("Connecting to: '");
-  Serial.print(s.ssid);
-  Serial.print("', with password: '");
-  Serial.print(s.password);
-  Serial.print("'");
-  
-  //while (WiFi.status() != WL_CONNECTED)
-  while (WiFiMulti.run() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // Enable Nextion communication
-  //sendToNextion("com_star");
-  //sendToNextion("bkcmd=0"); // No return values
-
-  //ticker.attach(5, ticker_updateDataFromApi);
+    setText(txtChargeText, "WiFi connected!");
 }
 
-void loop() {
-    if((WiFiMulti.run() != WL_CONNECTED)) {
+void loop()
+{
+    if ((WiFiMulti.run() != WL_CONNECTED))
+    {
         delay(1000);
         return;
     }
@@ -183,60 +195,78 @@ void loop() {
     delay(60000);
 }
 
-
-
-
-
-
-
-
-void sendToNextion(String command) {
+void sendToNextion(String command)
+{
     Serial.print(command);
     Serial.write(nextionCommandEnd, sizeof(nextionCommandEnd));
 }
 
-
-void setText(String textBox, String text) {
+void setText(String textBox, String text)
+{
     sendToNextion(textBox + ".txt=\"" + text + "\"");
 }
 
-void setValue(String control, int val) {
+void setValue(String control, int val)
+{
     sendToNextion(control + ".val=" + String(val));
 }
 
-void setPicture(String control, int val) {
+void setPicture(String control, int val)
+{
     sendToNextion(control + ".pic=" + String(val));
 }
 
-void setBackColor(String control, String color) {
+void setBackColor(String control, int color)
+{
     sendToNextion(control + ".bco=" + color);
 }
 
+void setForeColor(String control, int color)
+{
+    sendToNextion(control + ".pco=" + color);
+}
 
-void updateScreen() {
+void updateScreen()
+{
+    // Send the Nextion end of command packet to "clear" the Nextion serial buffer
+    sendToNextion("");
+
+    // Update values
     setText(txtSoC, String(socValue) + "%");
     setValue(pbSoC, socValue);
     setText(txtTimeStamp, timeStamp);
-    setText(txtRange, String(range / 1000));
-    //setText(txtRangeAc, "AC " + String(rangeAc / 1000) + "KM");
+    setText(txtRangeAc, String(rangeAc / 1000));
+    setText(txtRangeNoAc, String(rangeNoAc / 1000));
 
+    // Car dashboard
     float socDiv = 12.0f / 100.0f;
     setPicture("page0", round(socValue * socDiv));
 
-    if (chargingStatus == "NORMAL_CHARGING") {
-      //setPicture(picChargingStatus, 1);
-      setText(txtChargingStatus, "CHARGING");
-      
-    } else if (chargingStatus == "NOT_CHARGING") {
-      //setPicture(picChargingStatus, 2);
-      setText(txtChargingStatus, "NOT CHARGING");
+    // Battery progress bar colors
+    if (pluginState == "CONNECTED")
+    {
+        setForeColor(pbSoC, socValue >= 80 ? greenColor : orangeColor);
     }
+    else
+    {
+        setForeColor(pbSoC, socValue <= 30 ? redColor : blueColor);
+    }
+
+    // Charging status text
+    if (chargingStatus != "NOT_CHARGING")
+    {
+        setText(txtChargeText, "Charging...");
+    }
+    else if (pluginState == "CONNECTED" && chargingStatus == "NOT_CHARGING")
+    {
+        setText(txtChargeText, "Plugged in...");
+    }
+    else
+    {
+        setText(txtChargeText, "Nissan Leaf");
+    }
+
+
+    // Charging status icon
+    setText(txtChargeIcon, chargingStatus == "NOT_CHARGING" ? "" : "^"); // '^' is the charge icon character in the font
 }
-
-
-
-
-
-
-
-
